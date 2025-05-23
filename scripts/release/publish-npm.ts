9816fb9b3574e7c865eb4981822a97ea613635ec
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-23 16:27:53 +0200

    add latest back

--- scripts/release/publish-npm.ts
+++ scripts/release/publish-npm.ts
@@ -15,9 +15,11 @@ async function fetchTagsFromRegistry(packageName: string) {
 async function getTag({
   name,
   version,
+  latest,
 }: {
   name: string
   version: string
+  latest: string
 }): Promise<string> {
   const preConfigPath = join(process.cwd(), '.changeset', 'pre.json')
 
@@ -92,6 +94,7 @@ async function publishNpm() {
     const tag = await getTag({
       name: pkgJson.name,
       version: pkgJson.version,
+      latest: tags.latest,
     })
 
     await execa('pnpm', ['publish', '--tag', tag], {
